9ea096e32d9b41591f0c6580eac23eb36ab7d44d
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-05 14:16:29 -0400

    Write merge sort

--- recursionMerge.js
+++ recursionMerge.js
@@ -1,8 +1,29 @@
-const mergeSort = (array, leftHalf, rightHalf)=> {
-    if (){
-        return
+const mergeSort = (array)=> {
+    if (array.length <= 1){
+        return array
     } else {
-        return 
+        let midPoint = Math.floor(array.length/2);
+        let leftHalf = array.slice(0, midPoint);
+        let rightHalf = array.slice(midPoint);
+
+        leftHalf = mergeSort(leftHalf);
+        rightHalf = mergeSort(rightHalf);
+
+        let sortedArray = [];
+        let leftIteration = 0;
+        let rightIteration = 0;
+
+        while (sortedArray.length < array.length) {
+            if (leftHalf[leftIteration] < rightHalf[rightIteration] || rightHalf[rightIteration] === undefined){
+                sortedArray.push(leftHalf[leftIteration]);
+                leftIteration++;
+            } else {
+                sortedArray.push(rightHalf[rightIteration]);
+                rightIteration++;
+            }
+        }
+        console.log(sortedArray);
+        return sortedArray
     }
 }
 
